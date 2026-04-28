726113ee16f9e8fc9e6c11806ab340a25d584d43
theg1239 <thegreatestdropper@gmail.com>
2026-04-28 17:44:26 +0530

    fix

--- app/(mod)/mod/layout.tsx
+++ app/(mod)/mod/layout.tsx
@@ -1,11 +1,9 @@
 import type { ReactNode } from "react";
-import { connection } from "next/server";
 
-export default async function ModeratorLayout({
+export default function ModeratorLayout({
     children,
 }: {
     children: ReactNode;
 }) {
-    await connection();
     return children;
 }
