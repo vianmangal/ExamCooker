abe6c363900bf8879c305aad90c30f6aa01f6305
theg1239 <thegreatestdropper@gmail.com>
2026-05-02 12:02:36 +0530

    fix: capacitor overscroll

--- mobile/android/app/src/main/java/in/acmvit/examcooker/MainActivity.java
+++ mobile/android/app/src/main/java/in/acmvit/examcooker/MainActivity.java
@@ -1,6 +1,7 @@
 package in.acmvit.examcooker;
 
 import android.os.Bundle;
+import android.view.View;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +9,6 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(NativeDownloadsPlugin.class);
         super.onCreate(savedInstanceState);
+        getBridge().getWebView().setOverScrollMode(View.OVER_SCROLL_NEVER);
     }
 }
